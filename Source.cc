bcdb1df9dccf0fd9c2491adf85e7c8a0e8e050a3
zeri91 <l.salerno91@gmail.com>
2020-01-10 20:33:05 +0100

    parametri e classi

--- Source.cc
+++ Source.cc
@@ -9,6 +9,7 @@ class Source : public cSimpleModule
   private:
     cMessage *sendMessageEvent;
     int nbGenMessages;
+    int priority;
 
   public:
     Source();
@@ -35,17 +36,21 @@ void Source::initialize()
 {
     sendMessageEvent = new cMessage("sendMessageEvent");
     scheduleAt(simTime(), sendMessageEvent);
+    priority = 0;
+    nbGenMessages = 0;
 }
 
 void Source::handleMessage(cMessage *msg)
 {
     ASSERT(msg == sendMessageEvent);
     int n=par("numberofpriority");
+    priority = rand()%n+1;
     char msgname[20];
     sprintf(msgname,"message-%d", ++nbGenMessages);
     cMessage *message = new cMessage(msgname);
-    message->setSchedulingPriority(rand()%n+1);
+    message->setSchedulingPriority(priority);
     send(message, "out");
-
-    scheduleAt(simTime()+par("interArrivalTime").doubleValue(), sendMessageEvent);
+    char param[20];
+    sprintf(param, "interArrivalTime%d", priority);
+    scheduleAt(simTime()+par(param).doubleValue(), sendMessageEvent);
 }
