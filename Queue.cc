b54620a7e22af31e119e2afbcd75b4446d9513dc
Davide <davidesx11@gmail.com>
2020-01-13 14:37:12 +0100

    aggiunti parametri dello scheduler, modificato file Queue.cc per permettere la gestione dello scheduler e le diverse classi con diversi service time

--- Queue.cc
+++ Queue.cc
@@ -8,13 +8,16 @@ class Queue : public cSimpleModule
   protected:
     cMessage *msgServiced;
     cMessage *endServiceMsg;
+    cMessage *endSlotClass;
     //one queue for each priority class
     cQueue * queue;
     int n;
+    int index;
     int queue_size;
     bool preemption;
 
     char name[10];
+    char param[20];
     char signalname[20];
     char statisticName[20];
     // line of code to get global variable ev
@@ -54,7 +57,10 @@ Queue::~Queue()
 
 void Queue::initialize()
 {
+    //inizializzo l'indice delle priorità
+    index=1;
     endServiceMsg = new cMessage("end-service");
+    endSlotClass= new cMessage("end-slot");
     //get preemption parameter
     preemption = par("preemption");
     //n represent the number of classes
@@ -110,37 +116,43 @@ void Queue::handleMessage(cMessage *msg)
 
         EV << "Completed service of " << msgServiced->getName() << endl;
         send(msgServiced, "out");
-
+        //TODO: vedere come fargli stamapare la classe di priorità
+        EV << "now we are serving users of class ";
         //Response time: time from msg arrival timestamp to time msg ends service (now)
         emit(responseTimeSignal[msgServiced->getSchedulingPriority()-1], simTime() - msgServiced->getTimestamp());
-        //check all the classes if the relative queue is empty
-        for(int i = 0; i < n; i++){
-                if (queue[i].isEmpty()) { // Empty queue, server goes in IDLE
-                    if(i==n-1){
+        //controllo se sono all'ultima classe se sono all'ultima riparto da 1
+        //altrimenti vado alla successiva
+        if(index==n){
+            index=1;
+        }else{
+            index++;
+        }
+        //TODO: in questo caso spreco uno slot da vedere se implementarla diversamente
+                if (queue[index].isEmpty()) { // Empty queue, server goes in IDLE
                         EV << "Empty queue, server goes IDLE" <<endl;
                         msgServiced = nullptr;
                         emit(busySignal, false);
-                    }
                 }
                 else
                 {
                     //queue[i] is the higher non null priority queue
-                    msgServiced = (cMessage *)queue[i].pop();
-                    emit(qlenSignal[i], queue[i].getLength()); //Queue length changed, emit new length!
+                    msgServiced = (cMessage *)queue[index].pop();
+                    emit(qlenSignal[index], queue[index].getLength()); //Queue length changed, emit new length!
 
                     //Waiting time: time from msg arrival to time msg enters the server (now)
                     emit(queueingTimeSignal[msgServiced->getSchedulingPriority()-1], simTime() - msgServiced->getTimestamp());
 
-                    EV << "Starting service of " << msgServiced->getName() << " with priority "<< msgServiced->getSchedulingPriority() << endl;
-                    simtime_t serviceTime = par("serviceTime");
-                    //scheduling a self message
+                    EV << "Starting service of " << msgServiced->getName() << " of class "<< msgServiced->getSchedulingPriority() << endl;
+                    //setto il parametro corretto
+                    sprintf(param, "serviceTime%d", msgServiced->getSchedulingPriority());
+                    simtime_t serviceTime = par(param);
+                    //scheduling a self message of end of service
                     scheduleAt(simTime()+serviceTime, endServiceMsg);
-                    //force the exit to the loop
-                    break;
+                    simtime_t slotTime=par("timeSlot");
+                    //scheduling a self message of end of slot
+
 
                 }
-
-       }
     }
     else { // Data msg has arrived
 
@@ -154,7 +166,9 @@ void Queue::handleMessage(cMessage *msg)
                 //emit(queueingTimeSignal, SIMTIME_ZERO);
 
                 EV << "Starting service of " << msgServiced->getName() <<" with priority " << msgServiced->getSchedulingPriority() << endl;
-                simtime_t serviceTime = par("serviceTime");;
+                //setto il parametro corretto
+                sprintf(param, "serviceTime%d", msgServiced->getSchedulingPriority());
+                simtime_t serviceTime = par(param);;
                 scheduleAt(simTime()+serviceTime, endServiceMsg);
                 emit(busySignal, true);
             }
@@ -173,26 +187,22 @@ void Queue::handleMessage(cMessage *msg)
                                 //put in service new high priority service
                                 msgServiced = msg;
                                 EV << "Preemption: Starting service of " << msgServiced->getName() <<" with priority " << msg->getSchedulingPriority() << endl;
-                                simtime_t serviceTime = par("serviceTime");;
+                                //setto il parametro corretto
+                                sprintf(param, "serviceTime%d", msgServiced->getSchedulingPriority());
+                                simtime_t serviceTime = par(param);;
                                 scheduleAt(simTime()+serviceTime, endServiceMsg);
                                 return;
                             }
                 }
                 // if there is not set the preemption or the message has lower priority
-                queue_size=par("queue_size");
                 // I check all priorities
                 for(int i=0; i<n; i++){
                     // I need to subtract 1 to priorities propriety
                     // because my queue starts from 0 while priorities starts from 1
                     if(msg->getSchedulingPriority()-1==i){
-                        if(queue[i].getLength()<queue_size){
                             EV << msg->getName() << " enters queue"<< endl;
                             queue[i].insert(msg);
                             emit(qlenSignal[i], queue[i].getLength()); //Queue length changed, emit new length!
-                        }
-                        else{
-                            EV << msg->getName() << " with priority " << msg->getSchedulingPriority() << " is dropped from the system due to queue full"<< endl;
-                        }
                     }
                 }
            }
